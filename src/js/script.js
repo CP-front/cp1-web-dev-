7e7f46de362136912cd26f0e5ec279bfe803b34e
Rodrigo <rodrigocardosotadeo@gmail.com>
2025-04-19 17:12:34 -0300

    resolução exercício 1

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+// EXERCÍCIO 1
+// 1 - Como declarar uma variável com valor undefined, vazio e com valor em JavaScript? Exemplifique e demonstre a saída do valor da variável! 
+let nome;
+console.log(nome);
+let idade = "";
+console.log(idade);
+let valor = 10;
+console.log(valor);
+
